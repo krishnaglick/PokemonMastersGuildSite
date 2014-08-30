60cc60852cc5256baddf1afe8382e3e2425fb9e3
Krishna Glick <unstoppable411@gmail.com>
2014-08-30 00:55:21 -0400

    Added WoWSharp but the API calls appear to be taking literally forever. Will fix later?

--- PokemonMastersGuildSite/Controllers/GuildRosterController.cs
+++ PokemonMastersGuildSite/Controllers/GuildRosterController.cs
@@ -1,19 +1,24 @@
 ﻿using System.Web;
 using System.Web.Mvc;
+using WOWSharp.Community;
+using WOWSharp.Community.Wow;
 
 namespace PokemonMastersGuildSite.Controllers
 {
     public class GuildRosterController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(string Name)
         {
-            return View();
-        }
-
-        public ActionResult Welcome(string name)//, int numTimes = 1)
-        {
-            ViewBag.Message = "Hello " + name;
-            //ViewBag.NumTimes = numTimes;
+            WowClient wc = new WowClient(Region.US);
+            if (Name == null)
+            {
+                //var bs = wc.GetGuildAsync("Blackrock", "Pokemon Masters", GuildFields.Members).Result;
+                //ViewBag.Roster = bs;
+            }
+            else
+            {
+                ViewBag.Member = wc.GetCharacterAsync("Blackrock", Name, CharacterFields.All).Result;
+            }
 
             return View();
         }
